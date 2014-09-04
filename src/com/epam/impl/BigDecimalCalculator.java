8e1d798b2c0175bc8920cff2023e047a361e8683
Anton Spirchonak <Anton_Spirchonak@epam.com>
2014-09-04 09:59:09 +0300

    task03

--- src/com/epam/impl/BigDecimalCalculator.java
+++ src/com/epam/impl/BigDecimalCalculator.java
@@ -1,7 +1,31 @@
 package com.epam.impl;
 
+import com.epam.Calculator;
+
+import java.math.BigDecimal;
+
 /**
  * Created by Anton_Spirchonak on 9/4/2014.
  */
-public class BigDecimalCalculator {
+public class BigDecimalCalculator implements Calculator<BigDecimal> {
+
+    @Override
+    public Long add(Long first, Long second) {
+        return first + second;
+    }
+
+    @Override
+    public Long diff(Long first, Long second) {
+        return first - second;
+    }
+
+    @Override
+    public BigDecimal multiple(BigDecimal first, BigDecimal second) {
+        return first.multiply(second);
+    }
+
+    @Override
+    public BigDecimal divide(BigDecimal first, BigDecimal second) {
+        return first.divide(second);
+    }
 }
